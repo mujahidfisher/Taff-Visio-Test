ea0c6018255f846cd2722b289a54a7327b04493c
unknown <fishermujahid@gmail.com>
2024-02-23 11:35:34 +0200

    update

--- code.js
+++ code.js
@@ -25,3 +25,29 @@ $(document).ready(function(){
             });
   });
 
+// $(document).ready(function () {
+//     var currentIndex = 0;
+//     function showSlide(index) {
+//       $(".interiorImg").hide();
+//       $(".interiorImg:eq(" + index + ")").fadeIn();
+//       $(".indicator").removeClass("active");
+//       $(".indicator:eq(" + index + ")").addClass("active");
+//     }
+//     function nextSlide() {
+//       currentIndex = (currentIndex + 1) % $(".interiorImg").length;
+//       showSlide(currentIndex);
+//     }
+//     showSlide(currentIndex);
+//     var intervalId = setInterval(function () {
+//       nextSlide();
+//     }, 6000);
+//     $(".next").on("click", function () {
+//       clearInterval(intervalId);
+//       nextSlide();
+//     });
+//     $(".autoplay-indicators .indicator").on("click", function () {
+//       clearInterval(intervalId);
+//       currentIndex = $(this).index();
+//       showSlide(currentIndex);
+//     });
+//   });
